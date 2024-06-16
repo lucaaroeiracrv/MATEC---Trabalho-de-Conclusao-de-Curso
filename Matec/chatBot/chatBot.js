6cfdd3b88b2393b4d52f88177567e468a50a5ea1
lucaaroeiracrv <lucaaroeira@gmail.com>
2024-06-16 17:31:39 -0300

    fix: return chatbot

--- Matec/chatBot/chatBot.js
+++ Matec/chatBot/chatBot.js
@@ -1,17 +1,16 @@
+import readline from "readline";
 import fs from "fs";
 import { mostProbablyQuestion, todosBancos } from "./possiveisPerguntas";
 
 // Lê a pergunta do arquivo
-const pergunta = fs.readFileSync("question.txt", "utf8");
+let pergunta = fs.readFileSync("input.txt", "utf-8");
 let palavrasPergunta;
 
 // Remove um ponto de interrogação do final da pergunta, se houver
 if (pergunta[pergunta.length - 1] === "?") {
   pergunta = pergunta.slice(0, -1);
-  palavrasPergunta = pergunta.toLowerCase().split(" ");
-} else {
-  palavrasPergunta = pergunta.toLowerCase().split(" ");
 }
+palavrasPergunta = pergunta.toLowerCase().split(" ");
 
 // Função para comparar as palavras da pergunta com um banco de palavras possíveis
 function compararPergunta(banco) {
@@ -19,7 +18,7 @@ function compararPergunta(banco) {
   for (let i = 0; i < tamanhoBanco; ++i) {
     palavrasPergunta.forEach((palavra) => {
       if (banco.probablyWords[i] === palavra) {
-        banco.matched++;
+        banco.matched++; // Incrementa o contador de correspondências se uma palavra da pergunta coincide com uma palavra do banco
       }
     });
   }
@@ -28,15 +27,15 @@ function compararPergunta(banco) {
 // Função para comparar a pergunta com todos os bancos de palavras possíveis
 function compararTodos() {
   todosBancos.forEach((banco) => {
-    compararPergunta(banco);
+    compararPergunta(banco); // Chama a função compararPergunta para cada banco de palavras
   });
 }
 
 // Chama a função para comparar a pergunta com todos os bancos de palavras possíveis
 compararTodos();
 
-// Deduz a resposta mais provável
-const resposta = mostProbablyQuestion();
+// Captura a resposta mais provável
+let resposta = mostProbablyQuestion();
 
-// Salva a resposta em um arquivo
-fs.writeFileSync("answer.txt", resposta);
+// Escreve a resposta em um arquivo de saída
+fs.writeFileSync("output.txt", resposta);
--- Matec/chatBot/possiveisPerguntas.js
+++ Matec/chatBot/possiveisPerguntas.js
@@ -1,47 +1,26 @@
-function showAnswer(index) {
-  let response;
+export function showAnswer(index) {
   switch (index) {
     case 0:
-      response =
-        "Um pH alto na água pode ser corrigido ajustando-o para um nível mais neutro, geralmente entre 6,5 e 8,5, usando produtos como ácido cítrico, ácido ascórbico ou bicarbonato de sódio. Recomenda-se testar o pH da água regularmente para garantir que esteja dentro da faixa adequada para consumo.";
-      break;
+      return "Um pH alto na água pode ser corrigido ajustando-o para um nível mais neutro, geralmente entre 6,5 e 8,5, usando produtos como ácido cítrico, ácido ascórbico ou bicarbonato de sódio. Recomenda-se testar o pH da água regularmente para garantir que esteja dentro da faixa adequada para consumo.";
     case 1:
-      response =
-        "O gosto de ferrugem geralmente indica a presença de ferro na água. Isso pode ser resolvido instalando um sistema de filtração de água que seja eficaz na remoção de ferro, como um filtro de óxido de ferro ou um filtro de mangânese verde. Um pré-tratamento, como a oxidação do ferro, também pode ser necessário em alguns casos.";
-      break;
+      return "O gosto de ferrugem geralmente indica a presença de ferro na água. Isso pode ser resolvido instalando um sistema de filtração de água que seja eficaz na remoção de ferro, como um filtro de óxido de ferro ou um filtro de mangânese verde. Um pré-tratamento, como a oxidação do ferro, também pode ser necessário em alguns casos.";
     case 2:
-      response =
-        "Um cheiro estranho na água pode ser causado por várias razões, incluindo a presença de sulfeto de hidrogênio (cheiro de ovo podre) ou contaminação por compostos orgânicos. Um teste de qualidade da água pode identificar a causa específica, permitindo a implementação de medidas corretivas apropriadas.";
-      break;
+      return "Um cheiro estranho na água pode ser causado por várias razões, incluindo a presença de sulfeto de hidrogênio (cheiro de ovo podre) ou contaminação por compostos orgânicos. Um teste de qualidade da água pode identificar a causa específica, permitindo a implementação de medidas corretivas apropriadas.";
     case 3:
-      response =
-        "Problemas de pressão de água podem ser causados por vários fatores, como vazamentos na linha de água, obstruções nas tubulações ou problemas com o sistema de bombeamento. Verificar vazamentos, limpar ou substituir filtros e inspecionar a bomba d'água são etapas iniciais para diagnosticar e resolver o problema.";
-      break;
+      return "Problemas de pressão de água podem ser causados por vários fatores, como vazamentos na linha de água, obstruções nas tubulações ou problemas com o sistema de bombeamento. Verificar vazamentos, limpar ou substituir filtros e inspecionar a bomba d'água são etapas iniciais para diagnosticar e resolver o problema.";
     case 4:
-      response =
-        "Uma cor escura na água pode ser indicativa de sedimentos, minerais dissolvidos ou até mesmo contaminantes orgânicos ou inorgânicos. Um teste de qualidade da água pode ajudar a identificar a causa exata, permitindo a implementação de medidas corretivas apropriadas, como a instalação de um filtro de água adequado.";
-      break;
+      return "Uma cor escura na água pode ser indicativa de sedimentos, minerais dissolvidos ou até mesmo contaminantes orgânicos ou inorgânicos. Um teste de qualidade da água pode ajudar a identificar a causa exata, permitindo a implementação de medidas corretivas apropriadas, como a instalação de um filtro de água adequado.";
     case 5:
-      response =
-        "Sinais de contaminação bacteriana na água incluem odor desagradável, gosto estranho, mudanças na cor da água e a presença de partículas visíveis. Um teste de água específico para bactérias pode confirmar a presença delas, e a desinfecção da água usando cloro ou outros métodos adequados pode ser necessária para eliminar a contaminação.";
-      break;
+      return "Sinais de contaminação bacteriana na água incluem odor desagradável, gosto estranho, mudanças na cor da água e a presença de partículas visíveis. Um teste de água específico para bactérias pode confirmar a presença delas, e a desinfecção da água usando cloro ou outros métodos adequados pode ser necessária para eliminar a contaminação.";
     case 6:
-      response =
-        "Para eliminar o gosto de cloro da água da torneira, você pode usar um filtro de carvão ativado, que absorve o cloro e outros compostos orgânicos, ou deixar a água em um recipiente aberto por algumas horas para permitir que o cloro evapore naturalmente.";
-      break;
+      return "Para eliminar o gosto de cloro da água da torneira, você pode usar um filtro de carvão ativado, que absorve o cloro e outros compostos orgânicos, ou deixar a água em um recipiente aberto por algumas horas para permitir que o cloro evapore naturalmente.";
     case 7:
-      response =
-        "Para testar a presença de chumbo na água, você pode usar kits de teste específicos para chumbo disponíveis em lojas de ferragens ou solicitar um teste de qualidade da água em um laboratório certificado. Esses testes podem detectar até mesmo pequenas quantidades de chumbo na água.";
-      break;
+      return "Para testar a presença de chumbo na água, você pode usar kits de teste específicos para chumbo disponíveis em lojas de ferragens ou solicitar um teste de qualidade da água em um laboratório certificado. Esses testes podem detectar até mesmo pequenas quantidades de chumbo na água.";
     case 8:
-      response =
-        "Existem vários métodos eficazes para filtrar água em casa, incluindo sistemas de osmose reversa, que removem uma ampla gama de contaminantes, filtros de carvão ativado, que eliminam sabores e odores indesejados, filtros de cerâmica, que retêm bactérias e partículas, e filtros de gravidade, que são simples de usar e podem ser eficazes na remoção de sedimentos e contaminantes. A escolha do método depende das necessidades específicas de filtragem e da qualidade da água local";
-      break;
+      return "Existem vários métodos eficazes para filtrar água em casa, incluindo sistemas de osmose reversa, que removem uma ampla gama de contaminantes, filtros de carvão ativado, que eliminam sabores e odores indesejados, filtros de cerâmica, que retêm bactérias e partículas, e filtros de gravidade, que são simples de usar e podem ser eficazes na remoção de sedimentos e contaminantes. A escolha do método depende das necessidades específicas de filtragem e da qualidade da água local";
     default:
-      response =
-        "Não entendi a sua pergunta, por favor reformule a sua pergunta ou entre em contato com o a empresa";
+      return "Não entendi a sua pergunta, por favor reformule a sua pergunta ou entre em contato com a empresa";
   }
-  return response;
 }
 
 export function mostProbablyQuestion() {
